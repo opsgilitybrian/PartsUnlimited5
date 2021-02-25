eab2a243c43a1e20bf9abbcd5ac0c3d7a4ae6cad
Brian L. Gorman <bgorman@opsgility.com>
2021-02-25 16:51:11 -0600

    updates to the site, have products working but needs security and also needs to wire up stores and store products if we want to go to that level.  Also no cart/shopping/search wired up at this point

--- PartsUnlimited5.Web/Controllers/InventoryController.cs
+++ PartsUnlimited5.Web/Controllers/InventoryController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using PartsUnlimited5.Web.Data;
+using PartsUnlimited5.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,34 +23,46 @@ namespace PartsUnlimited5.Web.Controllers
 
         public async Task<IActionResult> Browse(string category)
         {
-            var categories = _context.Categories.AsNoTracking().ToList();
-            
-            int categoryId = 0;
+            var categories = _context.Categories.Include(y => y.Products).AsNoTracking().ToList();
+            var products = new List<Product>();
+            Category cat = new Category();
             switch (category)
             {
                 case "Brakes":
-                    categoryId = categories.SingleOrDefault(x => x.Name.ToLower().Equals("brakes"))?.Id ?? 0;
+                    cat = categories.SingleOrDefault(x => x.Name.ToLower().Equals("brakes"));
                     break;
                 case "Lighting":
-                    categoryId = categories.SingleOrDefault(x => x.Name.ToLower().Equals("lighting"))?.Id ?? 0;
+                    cat = categories.SingleOrDefault(x => x.Name.ToLower().Equals("lighting"));
                     break;
                 case "WheelsAndTires":
-                    categoryId = categories.SingleOrDefault(x => x.Name.ToLower().Equals("wheelsandtires"))?.Id ?? 0;
+                    cat = categories.SingleOrDefault(x => x.Name.ToLower().Equals("wheelsandtires"));
                     break;
                 case "Batteries":
-                    categoryId = categories.SingleOrDefault(x => x.Name.ToLower().Equals("batteries"))?.Id ?? 0;
+                    cat = categories.SingleOrDefault(x => x.Name.ToLower().Equals("batteries"));
                     break;
                 case "Oil":
-                    categoryId = categories.SingleOrDefault(x => x.Name.ToLower().Equals("oil"))?.Id ?? 0;
+                    cat = categories.SingleOrDefault(x => x.Name.ToLower().Equals("oil"));
                     break;
                 case "More":
-                    categoryId = -1;
+                    cat = null;
+                    foreach (var c in categories)
+                    {
+                        if (c.Products != null && c.Products.Any())
+                        {
+                            products.AddRange(c.Products);
+                        }
+                    }
                     break;
                 default:
                     break;
             }
 
-            return View(categoryId);
+            if (cat != null && cat.Products != null && cat.Products.Any())
+            {
+                products = cat.Products;
+            }
+
+            return View(products);
         }
     }
 }
