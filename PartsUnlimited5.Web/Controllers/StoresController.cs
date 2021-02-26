9592ba15cec11367d0809797b5d647e59abbd56b
Brian L. Gorman <bgorman@opsgility.com>
2021-02-26 09:36:45 -0600

    updated to add stores controller

---
+++ PartsUnlimited5.Web/Controllers/StoresController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PartsUnlimited5.Web.Data;
+using PartsUnlimited5.Web.Models;
+
+namespace PartsUnlimited5.Web.Controllers
+{
+    public class StoresController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StoresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Stores
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Stores.Include(s => s.CreatedByUser).Include(s => s.LastModifiedByUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Stores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Stores
+                .Include(s => s.CreatedByUser)
+                .Include(s => s.LastModifiedByUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        // GET: Stores/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatedByUserId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["LastModifiedByUserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Stores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Id,IsActive,CreatedDate,CreatedByUserId,LastModifiedDate,LastModifiedByUserId")] Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(store);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedByUserId"] = new SelectList(_context.Users, "Id", "Id", store.CreatedByUserId);
+            ViewData["LastModifiedByUserId"] = new SelectList(_context.Users, "Id", "Id", store.LastModifiedByUserId);
+            return View(store);
+        }
+
+        // GET: Stores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Stores.FindAsync(id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatedByUserId"] = new SelectList(_context.Users, "Id", "Id", store.CreatedByUserId);
+            ViewData["LastModifiedByUserId"] = new SelectList(_context.Users, "Id", "Id", store.LastModifiedByUserId);
+            return View(store);
+        }
+
+        // POST: Stores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Id,IsActive,CreatedDate,CreatedByUserId,LastModifiedDate,LastModifiedByUserId")] Store store)
+        {
+            if (id != store.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(store);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StoreExists(store.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedByUserId"] = new SelectList(_context.Users, "Id", "Id", store.CreatedByUserId);
+            ViewData["LastModifiedByUserId"] = new SelectList(_context.Users, "Id", "Id", store.LastModifiedByUserId);
+            return View(store);
+        }
+
+        // GET: Stores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Stores
+                .Include(s => s.CreatedByUser)
+                .Include(s => s.LastModifiedByUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        // POST: Stores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var store = await _context.Stores.FindAsync(id);
+            _context.Stores.Remove(store);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StoreExists(int id)
+        {
+            return _context.Stores.Any(e => e.Id == id);
+        }
+    }
+}
